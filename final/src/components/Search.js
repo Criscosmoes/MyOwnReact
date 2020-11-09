b69b715683509daefd5ea5eff8b7e22be1eb8c99
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-11-09 00:49:18 -0800

    added framer motion aka goat

--- final/src/components/Search.js
+++ final/src/components/Search.js
@@ -1,95 +1,96 @@
-import React from 'react'
-import styled from 'styled-components'; 
+import React from "react";
+import styled from "styled-components";
 
-import { Link } from 'react-router-dom'; 
+import { Link } from "react-router-dom";
 
-import { connect } from 'react-redux'; 
-import { fetchTrailers } from '../actions'; 
+import { connect } from "react-redux";
+import { fetchTrailers } from "../actions";
 
+import { motion } from "framer-motion";
 
 const StyledSearch = styled.div`
+  h1 {
+    margin: 2%;
+  }
 
-* {
-    border: 2px solid gray; 
-}
-
-h1 {
-    margin: 2%; 
-}
-
-.movie-container {
+  .movie-container {
     display: flex;
-    justify-content: space-around; 
-    align-items: center; 
-    flex-direction: column; 
-}
-
-img {
-    width: 150px; 
-    height: 200px; 
-}
-
-.movie {
-    display: flex; 
-    justify-content: center; 
-    align-items: center; 
-    margin: 2%; 
-}
-
-p {
-    text-align: center; 
-    overflow-y: auto; 
-    height: 16vh; 
-}
-
-h2 {
-    font-size: 1.9rem;
-}
-
-.info {
-    display: flex; 
-    justify-content: space-evenly; 
+    justify-content: space-around;
     align-items: center;
-    flex-direction: column; 
-    text-align: center; 
-    height: 100%; 
-}
+    flex-direction: column;
+  }
 
-.info > * {
+  img {
+    width: 150px;
+    height: 200px;
+  }
+
+  .movie {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 2%;
+  }
+
+  p {
+    text-align: center;
+    overflow-y: auto;
+    height: 16vh;
+  }
+
+  h2 {
+    font-size: 1.9rem;
+  }
+
+  .info {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    height: 100%;
     margin: 1%; 
-}
-`
+  }
 
-const Search = ({movies, fetchTrailers}) => {
+  .info > * {
+    margin: 1%;
+  }
+`;
 
-    const filteredList = movies.filter(cur => cur.poster_path !== null); 
+const Search = ({ movies, fetchTrailers }) => {
+  const filteredList = movies.filter((cur) => cur.poster_path !== null);
 
-
-    const renderedList = filteredList.map(cur => {
-        return (
-            <div className="movie">
-                <img src={`https://image.tmdb.org/t/p/original/${cur.poster_path}`} />
-                <div className="info">
-                    <h2>{cur.title}</h2>
-                    <p>{cur.overview}</p>
-                    <Link to={`/movie/${cur.id}`}><button onClick={() => fetchTrailers(cur.id)}>Watch Trailer</button></Link>
-                </div>
-            </div>
-        )
-    })
-
+  const renderedList = filteredList.map((cur) => {
     return (
-        <StyledSearch>
-            <div className="movie-container">
-                <h1>Results: {movies.length}</h1>
-                {renderedList}
-            </div>
-        </StyledSearch>
-    )
-}
+      <div className="movie">
+        <img src={`https://image.tmdb.org/t/p/original/${cur.poster_path}`} />
+        <div className="info">
+          <h2>{cur.title}</h2>
+          <p>{cur.overview}</p>
+          <Link to={`/movie/${cur.id}`}>
+            <button onClick={() => fetchTrailers(cur.id)}>Watch Trailer</button>
+          </Link>
+        </div>
+      </div>
+    );
+  });
 
-const mapStateToProps = state => {
+  return (
+    <StyledSearch>
+      <motion.div
+        exit={{ opacity: 0 }}
+        animate={{ opacity: 1, }}
+        initial={{ opacity: 0,}}
+        transition={{ delay: 1, duration: 0.6 }}
+        className="movie-container"
+      >
+        <h1>Results: {movies.length}</h1>
+        {renderedList}
+      </motion.div>
+    </StyledSearch>
+  );
+};
 
-}
+const mapStateToProps = (state) => {};
 
-export default connect(mapStateToProps, { fetchTrailers })(Search)
+export default connect(mapStateToProps, { fetchTrailers })(Search);
