2b439ccd0b18b93d4dd2052689bc42716da9e46d
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2020-11-15 18:45:43 -0800

    styled the search component

--- final/src/components/Search.js
+++ final/src/components/Search.js
@@ -12,7 +12,7 @@ import Spinner from './Spinner';
 
 const StyledSearch = styled.div`
   h1 {
-    margin: 2%;
+    margin: 1%;
   }
 
   .movie-container {
@@ -24,14 +24,20 @@ const StyledSearch = styled.div`
 
   img {
     width: 150px;
-    height: 200px;
+    height: 210px;
   }
 
   .movie {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin: 2%;
+    margin: 1%;
+    background: #1f2833; 
+    color: white; 
+    border: 4px solid white;
+    -moz-box-shadow: 5px 5px 40px black;
+    -webkit-box-shadow: 5px 5px 40px black;
+    box-shadow: 10px 10px 30px black;
   }
 
   p {
@@ -41,7 +47,7 @@ const StyledSearch = styled.div`
   }
 
   h2 {
-    font-size: 1.9rem;
+    font-size: 2rem;
   }
 
   .info {
@@ -57,6 +63,21 @@ const StyledSearch = styled.div`
   .info > * {
     margin: 1%;
   }
+
+  button {
+    background: #45a29e; 
+    color: white; 
+    padding: 2%; 
+    width: 100%; 
+  }
+
+  .overview {
+    margin: 5%
+  }
+
+  .link {
+    width: 70%; 
+  }
 `;
 
 const Search = ({ movies, fetchTrailers, isLoading }) => {
@@ -71,8 +92,8 @@ const Search = ({ movies, fetchTrailers, isLoading }) => {
         />
         <div className="info">
           <h2>{cur.title}</h2>
-          <p>{cur.overview}</p>
-          <Link to={`/movie/${cur.id}`}>
+          <p class="overview">{cur.overview}</p>
+          <Link className="link" to={`/movie/${cur.id}`}>
             <button onClick={() => fetchTrailers(cur.id, cur)}>Watch Trailer</button>
           </Link>
         </div>
@@ -91,10 +112,10 @@ const Search = ({ movies, fetchTrailers, isLoading }) => {
           exit={{ opacity: 0 }}
           animate={{ opacity: 1, }}
           initial={{ opacity: 0 }}
-          transition={{ duration: 1 }}
+          transition={{ duration: 1, delay: .5 }}
           className="movie-container"
         >
-          <h1>Results: {movies.length}</h1>
+          <h1>Results: {renderedList.length}</h1>
           {renderedList}
         </motion.div>
       )}
